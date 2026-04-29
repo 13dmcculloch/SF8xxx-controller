a6f699a1dfb7fbb66f98a25ebef87c0310cd8047
Douglas <142945292+13dmcculloch@users.noreply.github.com>
2026-04-29 11:56:37 +0100

    Update sf8.sh

--- sf8.sh
+++ sf8.sh
@@ -7,8 +7,9 @@ if [ ! -d "/run/screen/S-$LOGNAME"]; then
   echo "Screen run directory not present."
   exit
 fi
+# start detached sf8 status session
 if [ -z "$(ls "/run/screen/S-$LOGNAME" | grep sf8_status)" ]; then
-  screen -qdmS sf8_status
+  screen -qdmS sf8_status watch cat "$LOGFILE"
 fi
 
 ./SF8xxx-controller.py "$LOGFILE"
