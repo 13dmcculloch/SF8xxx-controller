e4aeaf9fa4ff2843db6ba16b630ae1aa3dc5a572
Douglas <142945292+13dmcculloch@users.noreply.github.com>
2026-04-29 12:11:01 +0100

    Update sf8.sh

--- sf8.sh
+++ sf8.sh
@@ -11,5 +11,8 @@ fi
 if [ -z "$(ls "/run/screen/S-$LOGNAME" | grep sf8_status)" ]; then
   screen -qdmS sf8_status watch cat "$LOGFILE"
 fi
+SESSION_NAME=$(ls "/run/screen/S-$LOGNAME" | grep sf8_status)
 
 ./SF8xxx-controller.py "$LOGFILE"
+
+screen -X -S "$SESSION_NAME" exit
