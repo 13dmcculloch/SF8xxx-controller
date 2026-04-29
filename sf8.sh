4fbc097e47e40c676a621c1cd5025d34ae21ced8
Douglas <142945292+13dmcculloch@users.noreply.github.com>
2026-04-29 11:49:16 +0100

    Update sf8.sh

--- sf8.sh
+++ sf8.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+LOGFILE="/tmp/sf8_status"
+
+# start sf8 status session if not in existence
+if [ ! -d "/run/screen/S-$LOGNAME"]; then
+  echo "Screen run directory not present."
+  exit
+fi
+if [ -z "$(ls "/run/screen/S-$LOGNAME" | grep sf8_status)" ]; then
+  screen -qdmS sf8_status
+fi
+
+./SF8xxx-controller.py "$LOGFILE"
